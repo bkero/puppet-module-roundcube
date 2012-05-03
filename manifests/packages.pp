24022c03eba6ea413777e304ed3823e39e261105
Ben Kero <ben.kero@gmail.com>
2012-05-03 13:09:04 -0700

    remove extraneous ]

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -22,7 +22,7 @@ class roundcube::packages inherits roundcube {
     }
 
     if $roundcube::db_backend == 'sqlite' {
-        package { 'php-mdb2-driver-sqlite']:
+        package { 'php-mdb2-driver-sqlite':
             ensure => $roundcube::package_ensure
         }
     }
